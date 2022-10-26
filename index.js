fc2c9865947521e471e4cb24757fe708204629f5
Vanortton <vanortton@gmail.com>
2022-10-26 11:06:20 -0300

    🚀 Update

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const inputColor = document.querySelector('input[data-color]')
+const rangeInputs = document.querySelectorAll('input[type="range"]')
+const numberInput = document.querySelector('input[type="number"]')
 const buttonLighten = document.querySelector('button[data-lighten]')
 const buttonDarken = document.querySelector('button[data-darken]')
 const message = document.querySelector('.results h3')
@@ -20,28 +22,50 @@ function hexToRgb(hex) {
     return result ? rgb : null
 }
 
-function changeColor(arrayRGB, por100) {
-    const porcentagem = (num, porcentagem) => num + (num * porcentagem)
+function changeColor(arrayRGB, per100) {
+    const percentage = (num, percentage) => num + (num * percentage)
 
     for (let i = 0; i < arrayRGB.length; i++) {
-        let result = Math.floor(porcentagem(arrayRGB[i], por100))
-        arrayRGB[i] = result > 255 ? result = 255 : result
+        let result = Math.floor(percentage(arrayRGB[i], per100))
+        arrayRGB[i] = result > 255 ? result = 255 :
+            result < 0 ? result = 0 : result
     }
 
     return [arrayRGB[0], arrayRGB[1], arrayRGB[2]]
 }
 
-function showColors({ arrReduceValue, regra, por100Value, messageText }) {
+function handleInputChange(e) {
+    let target = e.target
+    if (e.target.type !== 'range') {
+        target = document.getElementById('range')
+    }
+    const min = target.min
+    const max = target.max
+    const val = target.value
+
+    target.style.backgroundSize = (val - min) * 100 / (max - min) + '% 100%'
+}
+
+rangeInputs.forEach(input => {
+    input.addEventListener('input', handleInputChange)
+})
+
+numberInput.addEventListener('input', handleInputChange)
+
+function showColors({ arrReduceValue, regra, per100Value, messageText }) {
     ulToShow.innerHTML = ''
 
     let rgb = hexToRgb(inputColor.value)
-    rgb.reduce((prev, curr) => prev + curr, 0) === 0 ? rgb = [2, 2, 2] : ''
     let arrRgbReduce = arrReduceValue
 
     for (let i = 0; eval(regra); i++) {
-        const por100 = eval(por100Value)
+        const reduce0 = rgb.reduce((prev, curr) => prev + curr, 0) === 0
+        const reduce1 = rgb.reduce((prev, curr) => prev + curr, 0) === 3
+        const reduce2 = rgb.reduce((prev, curr) => prev + curr, 0) === 6
+
+        const per100 = eval(per100Value)
         const li = document.createElement('li')
-        let arrRGB = changeColor(rgb, por100)
+        let arrRGB = changeColor(rgb, per100)
 
         arrRgbReduce = arrRGB.reduce((prev, curr) => prev + curr, 0)
         stringRGB = `rgb(${arrRGB[0]}, ${arrRGB[1]}, ${arrRGB[2]})`
@@ -49,27 +73,56 @@ function showColors({ arrReduceValue, regra, por100Value, messageText }) {
         message.textContent = messageText
 
         li.style.background = stringRGB
-        li.innerHTML = `<span data-copyRGB>${stringRGB}</span>`
+        li.innerHTML = `<div data-rgb>${stringRGB}</div>`
         ulToShow.appendChild(li)
 
-        i += 9
+        if (reduce0) {
+            rgb = [1, 1, 1]
+        } else if (reduce1) {
+            rgb = [2, 2, 2]
+        } else if (reduce2) {
+            rgb = [3, 3, 3]
+        }
+
+        i += (numberInput.value - 1)
     }
+    const datas = document.querySelectorAll('ul li div')
+    datas.forEach(data => data.style.maxWidth = `calc(97vw / ${datas.length})`)
 }
 
 buttonLighten.onclick = () => {
     showColors({
         arrReduceValue: 0,
         regra: `arrRgbReduce < 765`,
-        por100Value: `i === 100 ? 1.0 : (i / 100)`,
-        messageText: 'Sua cor foi clareada'
+        per100Value: `i === 100 ? 1.0 : (i / 100)`,
+        messageText: 'Your light palette has been generated!'
     })
+    activeToCopy()
 }
 
 buttonDarken.onclick = () => {
     showColors({
         arrReduceValue: 765,
         regra: `arrRgbReduce > 0`,
-        por100Value: `i === 100 ? -1.0 : (i / 100) - ((i / 100) * 2)`,
-        messageText: 'Sua cor foi escurecida'
+        per100Value: `i === 100 ? -1.0 : (i / 100) - ((i / 100) * 2)`,
+        messageText: 'Your eyeshadow palette has been generated!'
+    })
+    activeToCopy()
+}
+
+function activeToCopy() {
+    const lis = document.querySelectorAll('li')
+    lis.forEach(li => {
+        li.onclick = () => {
+            let content = li.firstChild.textContent
+            navigator.clipboard.writeText(content)
+                .then(() => {
+                    alert('Text copied to clipboard...')
+                    console.log('Text copied to clipboard...')
+                })
+                .catch(err => {
+                    console.log('Something went wrong', err);
+                })
+        }
     })
 }
